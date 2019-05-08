6e78bacf2cc2ab93d1b6da38536939c131a286d5
Carlos Domínguez García <carlosdg97@gmail.com>
2019-05-08 20:09:33 +0100

    Feat: Time dimension sqlalchemy class
    - Class that represents the Time dimension of the star schema

--- Software/data_loader/parse.py
+++ Software/data_loader/parse.py
@@ -5,7 +5,7 @@ from glob import glob
 
 import pandas as pd
 
-from models import DimDate, Session
+from models import DimTime, Session
 
 
 def read_csvs(csv_file_paths, options):
@@ -54,7 +54,7 @@ def read_month_csvs(folder_path):
 
 if __name__ == "__main__":
     session = Session()
-    test = DimDate(date.today())
+    test = DimTime(23)
 
     try:
         session.add(test)
